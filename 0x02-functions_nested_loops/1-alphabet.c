9b40b379667ed2844983f546f32f40ff797e297d
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-12 10:14:44 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * A function that prints alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for(n = 'a'; n < 'z'; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
