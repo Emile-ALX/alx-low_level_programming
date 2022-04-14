fc4b3f081ac6d6e795547201e4a2dc10b4581b60
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-14 21:11:02 +0000

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * print_line -  checks for checks for a digit (0 through 9).
- *   * @n: n -  Variable
- *    * Return: Always 0.
- *     */
+ * print_line -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
+ */
 void print_line(int n)
 {
-		int x;
+int x;
 
-			if (n > 0)
-					{
-								for (x = 1; x <= n; x++)
-											{
-															_putchar(95);
-																	}
-									}
-				_putchar('\n');
+if (n > 0)
+{
+for (x = 1; x <= n; x++)
+{
+_putchar(95);
+}
+}
+_putchar('\n');
 }
