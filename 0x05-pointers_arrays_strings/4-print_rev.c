65e7ee69447537a97c4456d0457478d94695c59b
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-20 21:05:06 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_rev - a function that prints a string,
+ * in reverse, followed by a new line.
+ * @s: s input
+ * Return: reverse a string
+ */
+
+void print_rev(char *s)
+{
+	int countfoward;
+	int i, countback;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		countfoward++;
+	}
+	for (countback = (countfoward - 1) ; countback >= 0; countback--)
+	{
+		_putchar(s[countback]);
+	}
+
+	_putchar('\n');
+}
