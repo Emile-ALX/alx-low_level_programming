ddd92c0d066c4bddd27265c753e139702673ae42
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-21 09:34:07 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
-	while (src[i])
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\n';
 	return (dest);
 }
