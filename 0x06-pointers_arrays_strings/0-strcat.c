6154a5d1cbf44a8feff47055a4f85eb2ec8d252b
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-24 10:58:57 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - a function that concatenates two strings
+ *
+ * Return: concatenated strings
+ */
+int main(void)
+{
+	char *dest;
+	char *src;
+
+	puts(dest, src);
+	return (0);
+}
