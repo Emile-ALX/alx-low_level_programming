9b08f193b8fa4e79cfbeaa1a4561185b21586b7e
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-24 20:38:53 +0000

    100-print_number.c

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,22 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * print_number -print number putchar
- *   * @n:integer
- *    * Return: void
- *     */
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
+ */
 void print_number(int n)
 {
-		unsigned int x = n;
+	unsigned int x = n;
 
-			if (n < 0)
-					{
-								_putchar('-');
-										x = -x;
-											}
-				if ((x / 10) > 0)
-							print_number(x / 10);
-
-					_putchar(x % 10 + '0');
+	if (n < 0)
+	{
+	_putchar('-');
+	x = -x;
+	}
+	if ((x / 10) > 0)
+	print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
-
