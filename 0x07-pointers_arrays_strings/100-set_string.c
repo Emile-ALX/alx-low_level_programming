bf674f38f7bdfb8dce05ba731f4e1d3a977fc708
Emile-ALX <shumbushoemilef1@mail.com>
2022-04-27 00:19:19 +0000

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set a pointer to a string value
+ * @s: destination
+ * @to: source
+ * Return: nothing to return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
