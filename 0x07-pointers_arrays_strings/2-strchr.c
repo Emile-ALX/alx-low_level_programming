7d2097d3a46e912f0a927371cd7f72d414008abc
Emile-ALX <shumbushoemilef1@mail.com>
2022-05-04 20:57:22 +0000

    changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - locates a character in string
  * @s: source string
  * @c: character to find
  * Return: retuns a lerocated character
  */
+int main (void)
+{	
+	char *s = "hello";
+	char *f;
+	f = _strchr(s, 'l');
+	return (0);
+}
 char *_strchr(char *s, char c)
 {
 	int s1 = 0, s2;
