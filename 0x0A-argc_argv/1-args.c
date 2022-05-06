8ec94d7bd96f5961a84c7eeb3260572f1b257a6d
Emile-ALX <shumbushoemilef1@mail.com>
2022-05-06 10:56:18 +0000

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,16 +8,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int i;
-
-	if (argc == 1)
 	printf("%d\n", argc - 1);
-	else
-	{
-	for (i = 0; *argv; i++, argv++)
-	printf("%d\n", i - 1);
-	}
 	return (0);
 }
