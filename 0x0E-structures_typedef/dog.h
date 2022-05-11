6e5695993b9797abc987ea424bcd0a7a60860b51
Emile-ALX <shumbushoemilef1@mail.com>
2022-05-11 13:12:42 +0000

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ * Description: describes a dog entities
+ */
 struct dog
 {
 	char * name;
 	float age;
 	char * owner;
 }
+#endif
