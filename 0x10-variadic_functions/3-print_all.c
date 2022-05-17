1d9a74e7f89e03e0a804c19f373eee172787a9b2
Emile-ALX <shumbushoemilef1@mail.com>
2022-05-17 14:47:07 +0000

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,24 +1,88 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-
+#include<stdio.h>
+#include<stdarg.h>
 /**
+ * p_char - .......
+ * @list: args values
+ * Return: void
  */
-void print_all(const char * const format, ...)
+
+void p_char(va_list list)
 {
-	va_list list;
-	unsigned int i;
+	printf("%c", va_arg(list, int));
+}
+/**
+ * p_string - print string
+ * @list:arg
+ * Return: void
+ */
+
+void p_string(va_list list)
+{
 	char *str;
 
-	va_start(list, format);
-	for (i = 0; i < format; i++)
+	str = va_arg(list, char*);
+	if (str == NULL)
+	str = "(nil)";
+	printf("%s", str);
+}
+/**
+ * p_integer - print integer
+ * @list:arg
+ * Return: void
+ */
+
+void p_integer(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+/**
+ * p_float - print float
+ * @list:arg
+ * Return: void
+ */
+
+void p_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * print_all - print everything
+ * @format:arg
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+
+{
+	unsigned int i, j;
+	t_print t[] = {
+	{"c", p_char},
+	{"s", p_string},
+	{"i", p_integer},
+	{"f", p_float},
+	{NULL, NULL}
+	};
+	va_list valist;
+	char *s = "";
+
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
 	{
-		str = va_arg(list, char *);
-		if (str)
-			printf(("(nil)"));
-		else
-			printf("%s", str);
+	j = 0;
+	while (t[j].x != NULL)
+	{
+	if (*(t[j].x) == format[i])
+	{
+		printf("%s", s);
+		t[j].T_func(valist);
+		s = ", ";
+		break;
 	}
-	va_end(list);
+		j++;
+	}
+		i++;
+	}
+	va_end(valist);
 	printf("\n");
 }
