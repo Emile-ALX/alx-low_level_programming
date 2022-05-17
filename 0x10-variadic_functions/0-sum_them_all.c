9ba398ce5ef5217aa72575ddc3161458284ae794
Emile-ALX <shumbushoemilef1@mail.com>
2022-05-17 08:04:18 +0000

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,8 +8,9 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	double sum = 0;
+	unsigned int i;
 	va_list list;
+	double sum = 0;
 
 	if (n == 0)
 	{
@@ -22,7 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
